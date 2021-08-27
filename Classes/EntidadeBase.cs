b5a946a3cb4e7eadc5a8101abc0de968ad7ede4f
JhonatanSiqueira <jhon.siqueiraz@gmail.com>
2021-08-27 14:44:35 -0300

    Initial commit

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace DIO.Projeto1
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace DIO.Projeto1
+{
+	public enum Genero
+	{
+		Acao = 1,
+		Aventura = 2,
+		Comedia = 3,
+		Terror = 4,
+		Drama = 5,
+		Espionagem = 6,
+		Faroeste = 7,
+		Fantasia = 8,
+		Ficcao_Cientifica = 9,
+		Suspense = 10,
+		Romance = 11
+		
+		 
+	}
+}
